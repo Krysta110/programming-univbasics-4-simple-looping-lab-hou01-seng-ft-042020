6062e109d8683a256a62f175aa9898f9979b238b
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-01 06:10:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,31 +8,31 @@ def loop_message_five_times(string)
   end
 end
 
-def loop_message_n_times(string,integer)
-  count = 1
-  while count <= integer do
-    puts string
-    count +=1
-  end
-end
+# def loop_message_n_times(string,integer)
+#   count = 1
+#   while count <= integer do
+#     puts string
+#     count +=1
+#   end
+# end
 
-def output_array(array)
-  index = 0
-  while index < array.length do
-    puts array[index]
-    index +=1
-  end
-end
-
-def return_string_array(array)
-  new_array = []
-  count = 0
-  while count < array.length do
-    new_array.push(array[count].to_s)
-    count +=1
-  end
-  return new_array
-end
-
-
-end
+# def output_array(array)
+#   index = 0
+#   while index < array.length do
+#     puts array[index]
+#     index +=1
+#   end
+# end
+# 
+# def return_string_array(array)
+#   new_array = []
+#   count = 0
+#   while count < array.length do
+#     new_array.push(array[count].to_s)
+#     count +=1
+#   end
+#   return new_array
+# end
+# 
+# 
+# end
