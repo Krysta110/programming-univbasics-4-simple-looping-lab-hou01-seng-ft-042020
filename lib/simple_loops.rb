ad779a1ac0e024c973b9ac6372774a3899dfcb13
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-01 06:05:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,8 +8,12 @@ def loop_message_five_times(string)
   end
 end
 
-def loop_message_n_times
-
+def loop_message_n_times(string,integer)
+  count = 1
+  while count <= integer
+    puts string
+    count +=1 
+  end
 end
 
 def output_array
